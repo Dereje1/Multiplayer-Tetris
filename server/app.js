55cb4d62644459ed414a69224e86bffb866df85d
Dereje1 <getahun.d@gmail.com>
2019-01-16 23:07:45 +0700

    setup eslint

--- server/app.js
+++ server/app.js
@@ -1,27 +1,30 @@
 const express = require('express');
-const app = express();
 const logger = require('morgan');
 
+const app = express();
+
+
 app.use(logger('dev'));
-app.get('/test',(req, res)=>{
-    res.json({message: 'Hello World!'});
-})
+app.get('/test', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
-  
-  // error handler
-app.use((err, req, res) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.json(err);
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err);
 });
+
 module.exports = app;
