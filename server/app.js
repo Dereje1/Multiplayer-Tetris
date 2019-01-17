8a040892839f0be47a7706489e0068c3d21687c6
Dereje1 <getahun.d@gmail.com>
2019-01-17 10:23:11 +0700

    setup: mongoose and connect

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const logger = require('morgan');
 
 const app = express();
-
-
 app.use(logger('dev'));
+
+require('./models/db');
+
 app.get('/test', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
---
+++ server/models/db.js
@@ -0,0 +1,20 @@
+// Establishes connection to the db
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.dbURI);
+
+// on success
+mongoose.connection.on('connected', () => console.log(`Mongoose connected on ${process.env.dbURI}`));
+
+// on error
+mongoose.connection.on('error', err => console.log(`Mongoose connection error ${err}`));
+
+// on disconnection
+mongoose.connection.on('disconnected', () => console.log('Mongoose connection terminated'));
+
+// When Node process ends kill db connection
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('App has terminated , Mongoose disconnected');
+  });
+});
