4851e584ea4dea360c942bfe2da8c711a6f1ff0b
Dereje1 <getahun.d@gmail.com>
2019-01-18 19:34:53 +0700

    feature: header with login button

---
+++ client/src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/header.css';
+
+const Header = () => (
+  <div id="header">
+    <button id="login" type="submit" />
+  </div>
+);
+
+export default Header;
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/header/header';
 import App from '../App';
 import routerTester from '../components/testRouter';
 
 const Router = () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={App} />
-      <Route Path="/test" component={routerTester} />
-    </Switch>
+    <React.Fragment>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route Path="/test" component={routerTester} />
+      </Switch>
+    </React.Fragment>
   </BrowserRouter>
 );
 
