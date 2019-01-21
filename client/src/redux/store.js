46abc7620797d727f126b74f842aaacb519c7665
Dereje1 <getahun.d@gmail.com>
2019-01-21 23:17:17 +0700

    feature: customize logger

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,7 +5,9 @@ import rootReducer from './reducers/index';
 // don't use logger for production
 const reduxLogger = process.env.NODE_ENV === 'development' ? require('redux-logger') : null;
 
-const middleware = reduxLogger ? [thunk, reduxLogger.logger] : [thunk];
+const middleware = reduxLogger ? [thunk, reduxLogger.createLogger({
+  predicate: (getState, action) => action.type !== 'SCREEN_UPDATE',
+})] : [thunk];
 
 const store = createStore(
   rootReducer,
