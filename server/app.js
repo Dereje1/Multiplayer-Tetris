238f37c19b5edafd0038dbc978100f6b8bc41e19
Dereje <getahun.d@gmail.com>
2021-04-18 13:13:43 -0400

    Deployment fail

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 
 const app = express();
@@ -14,8 +13,8 @@ app.use((req, res, next) => {
     res.redirect(`https://${req.header('host')}${req.url}`);
   } else next();
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieSession({
   maxAge: 21 * 24 * 60 * 60 * 1000,
   keys: [process.env.COOKIE_KEY],
